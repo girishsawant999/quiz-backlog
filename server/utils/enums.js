67e4a62a580a7c6c2994140e0a1abc05b97675bc
Kamlesh Kumar <Kamleshpra995@gmail.com>
2024-10-22 22:30:10 +0530

    Added Question Schema

--- server/utils/enums.js
+++ server/utils/enums.js
@@ -1,8 +1,25 @@
 
 const userRoles = {
-	ADMIN: 'Admin',
-	OPERATOR: 'Operator',
-	APPROVER: 'Approver'
+  ADMIN: 'Admin',
+  OPERATOR: 'Operator',
+  APPROVER: 'Approver'
 }
 
-module.exports = { userRoles };
+const questionDifficulty = {
+  EAST: 'Easy',
+  MEDIUM: 'Medium',
+  HARD: 'Hard'
+}
+
+const questionCategory = {
+  CRICKET: 'Cricket',
+  SCIENCE: 'Science',
+  MATH: 'Math',
+  GK: 'General Knowledge',
+  POLITICS: 'Politics',
+  FESTIWALS: 'Festiwals',
+  MOVIES: 'Movies',
+  APTITUDE: 'Aptitude'
+}
+
+module.exports = { userRoles, questionDifficulty, questionCategory };
