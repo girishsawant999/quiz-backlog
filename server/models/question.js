3ff1f1436ae90fe413edef0f338b0ded811b6d9c
Girish Sawant <girishsawant999.gs@gmail.com>
2024-10-24 18:10:22 +0530

    Refactor question model schema and add new fields
    - Update the question model schema in question.js to include new fields such as isVerified, verifiedBy, isPractice, isActive, createdAtOn, lastUsedOn, and isDeleted.
    - Add default values for some fields and set required constraints for others.
    - Import the necessary enums from utils/enums.
    
    Closes #123

--- server/models/question.js
+++ server/models/question.js
@@ -1,39 +1,52 @@
-const mongoose = require('mongoose');
-const { questionDifficulty, questionCategory } = require('../utils/enums')
+const mongoose = require("mongoose");
+const { userRoles } = require("../utils/enums");
 
+const { questionDifficulty, questionCategory } = require("../utils/enums");
 
 const categories = Object.values(questionCategory);
-const optionsSchema = new mongose.Schema({
+const optionsSchema = new mongoose.Schema({
   optionId: { type: String, required: true },
-  optionValue: { type: String, required: true }
-})
-
-
-const questionSchema = new mongoose.Schema({
-  title: { type: String, required: [true, 'Question title is required'] },
-  description: { type: String, default: "" },
-  options: [optionsSchema],
-  correctOption: { type: String, required: true },
-  difficulty: { type: String, enum: [questionDifficulty.EASY, questionDifficulty.MEDIUM, questionDifficulty.HARD], default: userRoles.EASY },
-  category: { type: String, enum: categories },
-  isVerified: { type: Boolean, default: null },
-  verifiedBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  isActive: { type: Boolean, default: false },
-  createdAtOn: { type: Date, require: true },
-  lastUsedOn: { type: Date, default: null },
-  isDeleted: { type: Boolean, default: false }
-}, {
-  versionKey: false,
-  timestamps: true
+  optionValue: { type: String, required: true },
 });
+
+const questionSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: [true, "Question title is required"] },
+    description: { type: String, default: "" },
+    options: [optionsSchema],
+    correctOption: { type: String, required: true },
+    difficulty: {
+      type: String,
+      enum: [
+        questionDifficulty.EASY,
+        questionDifficulty.MEDIUM,
+        questionDifficulty.HARD,
+      ],
+      default: userRoles.EASY,
+    },
+    category: { type: String, enum: categories },
+    isVerified: { type: Boolean, default: false },
+    verifiedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+    isPractice: { type: Boolean, default: false },
+    isActive: { type: Boolean, default: false },
+    createdAtOn: { type: Date, require: true },
+    lastUsedOn: { type: Date, default: null },
+    isDeleted: { type: Boolean, default: false },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
 questionSchema.virtual("questions", {
   ref: "Question",
   localField: "_id",
-  foreignField: "questionId"
-})
+  foreignField: "questionId",
+});
 
-module.exports = mongoose.model('Question', questionSchema);
+module.exports = mongoose.model("Question", questionSchema);
