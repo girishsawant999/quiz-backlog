f9f3fa08121cc5de7c7671fcd61a409256a8f3ae
Girish Sawant <girishsawant999.gs@gmail.com>
2024-11-08 05:39:33 +0530

    refactor: improve layout spacing in Users page for better visual consistency

--- client/src/Pages/Users/index.tsx
+++ client/src/Pages/Users/index.tsx
@@ -6,14 +6,14 @@ const Users = () => {
   const actionContainerRef = useRef<HTMLDivElement>(null);
 
   return (
-    <section className="grid grid-rows-[auto,1fr]">
+    <section className="grid grid-rows-[auto,1fr] gap-5">
       <PageHeader
         title="Users"
         description={"Here's a list of users in the system."}
         actionContainerRef={actionContainerRef}
       />
 
-      <main className="overflow-y-auto scrollbar-hidden p-6">
+      <main className="overflow-y-auto scrollbar-hidden px-6">
         <UsersTable actionContainerRef={actionContainerRef} />
       </main>
     </section>
