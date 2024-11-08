5e8c4acd511f9962c632d41c4ba3e03c84486ea9
Girish Sawant <girishsawant999.gs@gmail.com>
2024-11-09 03:48:16 +0530

    feat: :sparkles: Enhance QuestionSchema to include detailed category structure and enum for difficulty levels

--- client/src/Pages/Questions/schema.ts
+++ client/src/Pages/Questions/schema.ts
@@ -6,13 +6,27 @@ export const OptionSchema = z.object({
   _id: z.string(),
 });
 
+export const QUESTION_DIFFICULTIES = {
+  EASY: "Easy",
+  MEDIUM: "Medium",
+  HARD: "Hard",
+};
+
 export const QuestionSchema = z.object({
   title: z.string(),
   description: z.string(),
   options: z.array(OptionSchema),
   correctOption: z.string(),
-  difficulty: z.string(),
-  category: z.string(),
+  difficulty: z.enum(["Easy", "Medium", "Hard"]),
+  category: z.object({
+    _id: z.string(),
+    category: z.string(),
+    description: z.string(),
+    isActive: z.boolean(),
+    isDeleted: z.boolean(),
+    createdAt: z.coerce.date(),
+    updatedAt: z.coerce.date(),
+  }),
   isVerified: z.boolean(),
   verifiedBy: z.null(),
   isPractice: z.boolean(),
